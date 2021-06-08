9c3e177b35313fec94d378a1141c00b8a9e3b23b
jamesfyp <hotjameschang@gmail.com>
2021-06-08 10:04:20 +0800

    币种列表

---
+++ coinbase/cointype.go
@@ -0,0 +1,23 @@
+package coinbase
+
+// coin type list
+// https://github.com/satoshilabs/slips/blob/master/slip-0044.md
+
+const (
+	BTC = iota
+	BTCTest
+	LTC
+	DOGE // 3
+	RDD
+	DASH
+	PPC // 6
+	NMC
+	FTC
+	XCP // 9
+	BLK
+	NSR
+	NBT
+	MZC
+	VIA
+	//...
+)
